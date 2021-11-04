eed7f6df7d7debcfd0aec8617f14cf682496f743
Sergey Miyusov <sergey.miyusov@detego.com>
2021-11-04 11:35:30 +0300

    implement handler
    lessons 7-10

---
+++ Handlers/UseCases/Order/Queries/GetOrderById/GetOrderByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationServices.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Handlers.CqrsFramework;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Handlers.UseCases.Order.Queries.GetOrderById
+{
+    public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQuery, OrderDto>
+    {
+        private readonly IDbContext _dbContext;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IMapper _mapper;
+
+        public GetOrderByIdQueryHandler(IDbContext dbContext, ICurrentUserService currentUserService, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _currentUserService = currentUserService;
+            _mapper = mapper;
+        }
+
+        public async Task<OrderDto> HandleAsync(GetOrderByIdQuery request)
+        {
+            var result = await _dbContext.Orders
+                .Where(o => o.Id == request.Id)
+                .ProjectTo<OrderDto>(_mapper.ConfigurationProvider)
+                .SingleAsync();
+
+            return result;
+        }
+    }
+}
